4a1c0c49288b8aeec928332b15bb5b9c48158c7a
Hull-Coleen <ch.hull@verizon.net>
2020-06-06 12:40:32 -0400

    having to redo the entire project

---
+++ MyScriptureJournal/Models/JournalEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class JournalEntry
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Entry Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public string Verse { get; set; }
+        public string Notes { get; set; }
+    }
+
+}
