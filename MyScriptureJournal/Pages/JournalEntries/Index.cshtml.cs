e6a518101eb4b5f47358ff9f30fb8153a61fc93d
Hull-Coleen <ch.hull@verizon.net>
2020-06-06 20:04:19 -0400

    added correct link

--- MyScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
+++ MyScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
@@ -20,60 +20,39 @@ namespace My_Scripture_Journal.Pages.JournalEntries
         }
 
         public IList<JournalEntry> JournalEntry { get;set; }
-        [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
-        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+
         public SelectList Books { get; set; }
         [BindProperty(SupportsGet = true)]
         public string JournalEntryBook { get; set; }
         [BindProperty(SupportsGet = true)]
         public string newSearchString { get; set; }
-        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
-        public SelectList Notes { get; set; }
-        [BindProperty(SupportsGet = true)]
-        public string NoteKeyWord { get; set; }
+       
         [BindProperty(SupportsGet = true)] 
         public string BookSort { get; set; }
         [BindProperty(SupportsGet = true)]
         public string DateSort { get; set; }
-        // public string CurrentFilter { get; set; }
-        // public string CurrentSort { get; set; }
 
         public async Task OnGetAsync(string sortOrder)
         {
-       
-
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+   
             // Use LINQ to get list of genres.
             IQueryable<string> bookQuery = from e in _context.JournalEntry
-                                            orderby e.EntryDate
                                             orderby e.Book
                                             select e.Book;
-            IQueryable<string> notesQuery = from e in _context.JournalEntry
-                                            orderby e.EntryDate
-                                            orderby e.Book
-                                            select e.Notes;
 
             IQueryable<JournalEntry> Journal = from s in _context.JournalEntry
                                                select s;
-            if (!string.IsNullOrEmpty(SearchString))
-            {
-                Journal = Journal.Where(s => s.Book.Contains(SearchString));
-            }
+             
             if (!string.IsNullOrEmpty(JournalEntryBook))
             {
                 Journal = Journal.Where(x => x.Book == JournalEntryBook);
             }
             if (!string.IsNullOrEmpty(newSearchString))
-            {
-                Journal = Journal.Where(s => s.Notes.Contains(newSearchString));
-            }
-            if (!string.IsNullOrEmpty(NoteKeyWord))
-            {
-                Journal = Journal.Where(x => x.Notes == NoteKeyWord);
-            }
-
-            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
-            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+             {
+                 Journal = Journal.Where(s => s.Notes.Contains(newSearchString));
+             }
 
             switch (sortOrder)
             {
@@ -93,7 +72,6 @@ namespace My_Scripture_Journal.Pages.JournalEntries
 
             JournalEntry = await Journal.AsNoTracking().ToListAsync();
             Books = new SelectList(await bookQuery.Distinct().ToListAsync());
-            Notes = new SelectList(await notesQuery.Distinct().ToListAsync());
         }
     }
 }
